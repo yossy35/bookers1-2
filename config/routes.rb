48fa44a271b704b1a0b6f3031e8f4b4f8986f8ef
EC2 Default User <ec2-user@ip-172-31-46-251.ap-northeast-1.compute.internal>
2024-12-30 03:37:43 +0000

    bookers edit.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :books
   get '/top' => 'homes#top'
-  get 'books/:id' => 'books#show'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as:'edit_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
