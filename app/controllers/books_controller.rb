61e16796264c0c83613bd6051e7b00a946102aeb
EC2 Default User <ec2-user@ip-172-31-46-251.ap-northeast-1.compute.internal>
2025-01-01 22:28:04 +0000

    bookers destroyERROR

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,6 +26,12 @@ class BooksController < ApplicationController
     book.update(book_params)
     redirect_to book_path(book.id)
   end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
   
   private
   def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :books
   get '/top' => 'homes#top'
-  get 'books/:id' => 'books#show', as: 'book'
-  get 'books/:id/edit' => 'books#edit', as:'edit_book'
-  patch 'books/:id' => 'books#update', as: 'update_book'
+  get 'books/:id' => 'books#show'
+  get 'books/:id/edit' => 'books#edit'
+  patch 'books/:id' => 'books#update'
+  delete 'books/:id' => 'books#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
