e8c56a8e3aff371452d1365ab9970af33bf5ab6c
EC2 Default User <ec2-user@ip-172-31-46-251.ap-northeast-1.compute.internal>
2025-01-02 21:30:06 +0000

    bookers destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
+  get 'top' => 'homes#top'
   resources :books
-  get '/top' => 'homes#top'
-  get 'books/:id' => 'books#show'
-  get 'books/:id/edit' => 'books#edit'
-  patch 'books/:id' => 'books#update'
-  delete 'books/:id' => 'books#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
